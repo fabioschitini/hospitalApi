338b255d2c320d8bc1cc7e832753f5e1bb5acdd3
Fabio <Fabio@10.0.0.5>
2023-10-17 12:16:44 -0300

    adicionie o eurekaServer e o gateway

--- gateway/src/main/java/com/ifba/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/ifba/gateway/GatewayApplication.java
@@ -1,13 +1,13 @@
-package com.example.hospital;
+package com.ifba.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HospitalApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HospitalApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
